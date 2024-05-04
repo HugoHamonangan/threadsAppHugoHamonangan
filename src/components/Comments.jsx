526c10395dd231fbc646b5dff7339737bc0cab6e
HugoHamonangan <naberius103@gmail.com>
2024-05-04 10:00:34 +0700

    make test error

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -27,7 +27,7 @@ const Comments = ({ threadDetails, addComments, comment, setComment }) => {
           type="submit"
           className="w-full bg-blue-500 text-white p-2 mt-4 hover:bg-blue-600"
         >
-          Kirim
+          Kiri
         </button>
       </form>
     </div>
